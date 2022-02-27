241f12df81eafbb870381096b91d732ac7a7cdd2
misba0770 <80534061+misba0770@users.noreply.github.com>
2022-02-27 13:28:54 +0530

    kth largest element in array

---
+++ kLargestELements.java
@@ -0,0 +1,36 @@
+
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author misba zaidi
+ */
+public class kLargestELements {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)arr[i]=s.nextInt();
+        int k=s.nextInt();
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for(int i=0;i<n;i++){
+            pq.add(arr[i]);
+            if(pq.size()>k)pq.poll();
+        }
+        while(!pq.isEmpty()){
+            System.out.print(pq.poll()+" ");
+        }
+    }
+    
+}
